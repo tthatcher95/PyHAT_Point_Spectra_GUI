db301319ab0a45254d80fe0780e728538865d4bf
tisaconundrum2 <ngf4@nau.edu>
2017-08-29 10:51:27 -0700

    Return GUI save
    Former-commit-id: 617dded397f7695bce785173fe626ce6488babe6 [formerly d0bbae39c7fa5b36082208490a86af8352a94a62]
    Former-commit-id: cbbf0a205edbb0522790fc4f791a9870230ac275

---
+++ point_spectra_gui/future_/functions/PlotSpectra.py
@@ -0,0 +1,19 @@
+from Qtickle import Qtickle
+from point_spectra_gui.ui.PlotSpectra import Ui_Form
+
+
+class Ui_Form(Ui_Form):
+    uiID = 0
+
+    def setupUi(self, Form):
+        super().setupUi(Form)
+        self.connectWidgets()
+        Ui_Form.uiID += 1
+
+    def get_widget(self):
+        return self.groupBox
+
+    def connectWidgets(self):
+        self.qt = Qtickle.Qtickle(self)
+        self.qt.isGuiChanged(self.qt.guiSave)
+        print(Ui_Form.uiID)
---
+++ point_spectra_gui/future_/functions/SubmodelPredict.py
@@ -0,0 +1,18 @@
+from Qtickle import Qtickle
+from point_spectra_gui.ui.SubmodelPredict import Ui_Form
+
+
+class Ui_Form(Ui_Form):
+    uiID = 0
+
+    def setupUi(self, Form):
+        super().setupUi(Form)
+        self.connectWidgets()
+        Ui_Form.uiID += 1
+
+    def get_widget(self):
+        return self.groupBox
+
+    def connectWidgets(self):
+        self.qt = Qtickle.Qtickle(self)
+        self.qt.isGuiChanged(self.qt.guiSave)
