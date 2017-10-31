5e1bd9e0d08328141fcbb2c433600083c1bd5da7
Nicholas Finch <ngf4@nau.edu>
2017-10-30 18:35:21 -0700

    Add comments
    Former-commit-id: 3692f48c8a0c0677df982a3e440a513009f972b1

--- install_travis.sh
+++ install_travis.sh
@@ -1,9 +1,11 @@
-export PATH="$HOME/miniconda/bin:$PATH"
-conda config --set always_yes yes --set changeps1 no
-while read requirement; do conda install --yes $requirement || pip install $requirement; done < requirements.txt
-git clone --depth=50 --branch=master https://github.com/USGS-Astrogeology/PySAT.git
-cd PySAT
-python setup.py install
-cd ..
-cd PySAT_Point_Spectra_GUI
-python setup.py install
+export PATH="$HOME/miniconda/bin:$PATH"                                             # Set up the miniconda path
+conda config --set always_yes yes --set changeps1 no                                # Always say yes to questions
+while read requirement; do                                                          # read through all the requirements
+  conda install --yes $requirement || pip install $requirement;                     # conda install or pip install
+  done < requirements.txt                                                           # get the information from requirements.txt
+git clone --depth=50 --branch=master https://github.com/USGS-Astrogeology/PySAT.git # clone the PySAT repo
+cd PySAT                                                                            # cd into it
+python setup.py install                                                             # setup install
+cd ..                                                                               # get out of the PySAT repo
+cd PySAT_Point_Spectra_GUI                                                          # go into PySAT_Point_Spectra_GUI
+python setup.py install                                                             # setup install
