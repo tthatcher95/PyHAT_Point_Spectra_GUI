362e6f137929bdf994e31a9e13d1231ad8c73662
Nicholas Finch <ngf4@nau.edu>
2017-11-01 12:38:07 -0700

    use environment.yml to get all dependencies
    Former-commit-id: ab68e1f267533bb09d8a4b7f5a8d849326232406

--- install_travis.sh
+++ install_travis.sh
@@ -3,9 +3,3 @@ conda config --set always_yes yes --set changeps1 no                            
 while read requirement; do                                                          # read through all the requirements
   conda install --yes $requirement || pip install $requirement;                     # conda install or pip install
   done < requirements.txt                                                           # get the information from requirements.txt
-git clone --depth=50 --branch=master https://github.com/USGS-Astrogeology/PySAT.git # clone the PySAT repo
-cd PySAT                                                                            # cd into it
-python setup.py install                                                             # setup install
-cd ..                                                                               # get out of the PySAT repo
-cd PySAT_Point_Spectra_GUI                                                          # go into PySAT_Point_Spectra_GUI
-python setup.py install                                                             # setup install
