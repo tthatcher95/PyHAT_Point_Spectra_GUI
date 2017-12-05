0d558456517d65ee03ef237bbc7ebe2f3c47bccb
Nicholas Finch <ngf4@nau.edu>
2017-12-05 13:55:45 -0700

    Temporarily remove selectiveRestore
    Former-commit-id: e388bdc2b85deb2147b46d9e40cd8840b80553dc [formerly 0ec8ba06f3b40f0d00653d6e1dd7e9c3908438dd]
    Former-commit-id: f7921d722968afe5da175460ae33aa99d5f81649

--- point_spectra_gui/core/MainWindow.py
+++ point_spectra_gui/core/MainWindow.py
@@ -539,9 +539,9 @@ class MainWindow(Ui_MainWindow, QtCore.QThread, Basics):
             name_ = type(self.widgetList[modules]).__name__
             s = time.time()
             print("{} Module is Running...".format(name_))
-            if dic is not None:
-                self.widgetList[modules].connectWidgets()
-                self.widgetList[modules].selectiveSetGuiParams(dic[modules + 1])
+            # if dic is not None:
+            #     self.widgetList[modules].connectWidgets()
+            #     self.widgetList[modules].selectiveSetGuiParams(dic[modules + 1])
             self.widgetList[modules].function()
             e = time.time()
             print("Module {} executed in: {} seconds".format(name_, e - s))
