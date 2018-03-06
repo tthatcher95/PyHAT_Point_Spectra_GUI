2aa082f566f216d4937244b6b84abd265c370814
Nicholas Finch <ngf4@nau.edu>
2018-03-06 12:51:27 -0700

    increase maximum width

--- point_spectra_gui/util/Modules.py
+++ point_spectra_gui/util/Modules.py
@@ -203,7 +203,7 @@ class Modules:
         :return:
         """
         comboBox.clear()
-        comboBox.setMaximumWidth(200)
+        comboBox.setMaximumWidth(400)
         comboBox.addItems(keyValues)
 
     @staticmethod
