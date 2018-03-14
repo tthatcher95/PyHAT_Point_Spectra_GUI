9e33e97d031559a98afa3b722e102740338dad63
Tisaconundrum2 <ngf4@nau.edu>
2018-03-14 15:02:24 -0700

    Debug discrepancy. It needed to be the other way around

--- point_spectra_gui/core/MainWindow.py
+++ point_spectra_gui/core/MainWindow.py
@@ -583,9 +583,9 @@ class MainWindow(Ui_MainWindow, QtCore.QThread, Modules):
         dic = self.getWidgetItems()
         for modules in range(self.leftOff, len(self.widgetList)):
             if self.debug:
-                self._logger(self.widgetList[modules].setup)
-            else:
                 self.widgetList[modules].setup()
+            else:
+                self._logger(self.widgetList[modules].setup)
             self.widgetList[modules].connectWidgets()
             self.widgetList[modules].selectiveSetGuiParams(dic[modules + 1])
 
