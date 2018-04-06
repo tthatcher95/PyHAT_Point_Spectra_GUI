bd396abbe8e2583b2c7296bc394bea31c097b1b5
Ryan <rbanderson@usgs.gov>
2018-04-06 16:33:52 -0700

    Get rid of unused mouse press message

--- point_spectra_gui/util/Modules.py
+++ point_spectra_gui/util/Modules.py
@@ -50,7 +50,8 @@ class Modules:
         modules from the UI, or insert modules, or copy modules.
         """
         # TODO Add mouse Event
-        print("Right Button Clicked {}".format(type(self).__name__))
+        pass
+        # print("Right Button Clicked {}".format(type(self).__name__))
 
     def get_widget(self):
         """
