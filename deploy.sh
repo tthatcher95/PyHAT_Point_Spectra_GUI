3c990a44e115bff9e42d00a4471a557d767f55a4
Thatcher <tthatcher84@gmail.com>
2020-04-08 12:40:16 -0700

    Update Travis

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ echo "Python version: $1"
 source deactivate
 conda install conda-build anaconda-client
 conda config --set anaconda_upload yes
-conda build --token $CONDA_UPLOAD_TOKEN --python $1 -c usgs-astrogeology recipe
+conda build --token $CONDA_UPLOAD_TOKEN --python $1 -c usgs-astrogeology RECIPE_PATH recipe
